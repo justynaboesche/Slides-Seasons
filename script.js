64a20e066a580c15f0aeb2f766c5a314fb8d03d1
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-15 12:37:12 +0100

    function handleBgColor created

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ function showCard() {
 		card.classList.remove('active');
 		this.classList.add('active');
 	});
+    handleBgColor(this)
+}
+
+const handleBgColor = (card) => {
+const season = card.getAttribute('data-season')
+body.setAttribute('data-bg', season)
 }
 
 cards.forEach(card => card.addEventListener('click', showCard));
