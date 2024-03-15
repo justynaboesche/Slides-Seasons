05e75af88cf65dad131e298698d0907477c90d57
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-15 12:27:48 +0100

    function showCard created

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Nic tu nie ma 😱')
+const body = document.querySelector('body');
+const cards = document.querySelectorAll('.card');
+
+
+function showCard() {
+	cards.forEach(card => {
+
+        console.log(this);
+		card.classList.remove('active');
+		this.classList.add('active');
+	});
+}
+
+cards.forEach(card => card.addEventListener('click', showCard));
